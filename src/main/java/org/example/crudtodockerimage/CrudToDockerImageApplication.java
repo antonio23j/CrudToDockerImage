76810fe5cbbed503a5bddad018d7f209a35d4efa
antonio23j <antonio.jaicari@gmail.com>
2024-03-20 15:11:31 +0100

    initial commit

---
+++ src/main/java/org/example/crudtodockerimage/CrudToDockerImageApplication.java
@@ -0,0 +1,13 @@
+package org.example.crudtodockerimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudToDockerImageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudToDockerImageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/crudtodockerimage/CrudToDockerImageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crudtodockerimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudToDockerImageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
